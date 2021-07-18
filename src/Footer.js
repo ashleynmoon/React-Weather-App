bdeb4b455ce73c077bc0d3f93371683a732fd549
Ashley Moon <79172865+ashleynmoon@users.noreply.github.com>
2021-07-17 22:24:44 -0400

    added bootstrap and fontawesome

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return <div className="text-center" id="open-source-link">
